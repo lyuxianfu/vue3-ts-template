b1dec403a460fc94e6110c3437244e7519caae82
lyuxianfu <562212133@qq.com>
2022-08-01 14:00:11 +0800

    feat： template

---
+++ ask.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+module.exports = [
+  {
+    name: 'name',
+    message: 'Please enter project name:',
+  },
+  {
+    name: 'description',
+    message: 'Please enter project description:',
+  },
+  {
+    name: 'author',
+    message: 'Please enter project author:',
+  },
+];
